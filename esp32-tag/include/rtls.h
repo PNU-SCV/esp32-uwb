1b5020d94c57c28e65a487900252419c3719d541
DevSWYoon <62388909+DevSWYoon@users.noreply.github.com>
2024-05-11 15:44:03 +0900

    Separate RTLS codes, Resetting Project Directory
    1. Separate RTLS codes
    - rtls.h, rtls.cpp
    
    2. Resetting Project Directory

---
+++ esp32-tag/include/rtls.h
@@ -0,0 +1,6 @@
+#ifndef __RTLS_H__
+#define __RTLS_H__
+
+void RTLS_Task(void *parameter);
+
+#endif
--- esp32-uwb/include/main.h
+++
@@ -1,8 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include "link.h"
-#include "rasp.h"
-#include "rtls.h"
-
-#endif
