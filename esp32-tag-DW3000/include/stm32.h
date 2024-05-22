ca4e012887cd39735ed6c69ba9a469bb2e927148
DevSWYoon <62388909+DevSWYoon@users.noreply.github.com>
2024-05-22 23:53:21 +0900

    Update stm32.h
    Change Pin-Map

--- esp32-tag-DW3000/include/stm32.h
+++ esp32-tag-DW3000/include/stm32.h
@@ -4,8 +4,8 @@
 #include "point.h"
 
 #define STM32_UART_BAUD 115200
-#define STM32_RX_PIN 4
-#define STM32_TX_PIN 5
+#define STM32_RX_PIN 16
+#define STM32_TX_PIN 17
 
 #define CMD_STOP (uint8_t)0x00
 #define CMD_FORWARD (uint8_t)0x01
