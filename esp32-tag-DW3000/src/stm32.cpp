e8ac0bdfa5b912744c7211c5f4223b45835fb6ae
DevSWYoon <62388909+DevSWYoon@users.noreply.github.com>
2024-08-22 13:38:44 +0900

    Update stm32.cpp

--- esp32-tag-DW3000/src/stm32.cpp
+++ esp32-tag-DW3000/src/stm32.cpp
@@ -9,22 +9,21 @@ extern TaskHandle_t RTLS_task_handle;
 extern TaskHandle_t rasp_recv_task_handle;
 extern TaskHandle_t rasp_send_task_handle;
 
-extern uint8_t rasp_cmd;
-extern uint8_t rasp_stat;
-
 HardwareSerial Stm32HwSerial(1);
 
-STM32RecvData stm32_recieve_data;
-STM32SendData stm32_send_data;
+STM32RecvData stm32RecieveData = {0};
+STM32SendData stm32SendData = {0};
 
 extern SemaphoreHandle_t stm32_recv_data_semaphore;
 extern SemaphoreHandle_t rasp_recv_data_semaphore;
 
-extern SemaphoreHandle_t stm32_send_flag_semaphore;
+extern Point2D tagPosition;
+extern float tagAngle;
 
-extern Point2D target_loc;
-extern Point2D tag_position;
-extern float tag_angle;
+extern Point2D destPoint;
+extern uint8_t raspCmd;
+
+uint8_t stm32Status = 0;
 
 float getAngle(Point2D target, Point2D cur);
 
@@ -35,27 +34,31 @@ void stm32RecvTask(void *parameter)
     /* Task pending (Rasp Recv 에서 이 태스크를 트리거할 때까지 대기) */
     ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
 
-    if(xSemaphoreTake(stm32_recv_data_semaphore, portMAX_DELAY) == pdFALSE) continue;
-
     Serial.println("stm32RecvTask");
 
     if (Stm32HwSerial.available() >= sizeof(STM32RecvData)) 
     {
-      Stm32HwSerial.readBytes((char*)&stm32_recieve_data, sizeof(STM32RecvData));
+      Stm32HwSerial.readBytes((char*)&stm32RecieveData, sizeof(STM32RecvData));
 
-      rasp_stat = stm32_recieve_data.status;
+      if(xSemaphoreTake(stm32_recv_data_semaphore, portMAX_DELAY) == pdFALSE) continue;
+
+      stm32Status = stm32RecieveData.status;
+
+      xSemaphoreGive(stm32_recv_data_semaphore);
 
       Serial.print("STM32 Recv: ");
-      Serial.println(rasp_stat);
+      Serial.println(stm32Status);
     }
-
-    xSemaphoreGive(stm32_recv_data_semaphore);
   }
 }
 
 /* 송신 태스크 */
 void stm32SendTask(void *parameter) 
 {
+  uint8_t stm32_status, rasp_cmd;
+  Point2D tag_position, dest_point;
+  float tag_angle;
+
   while (true) 
   {
     /* Task pending (raspRecvTask에서 이 태스크를 트리거할 때까지 대기) */
@@ -63,43 +66,50 @@ void stm32SendTask(void *parameter)
 
     if(xSemaphoreTake(stm32_recv_data_semaphore, portMAX_DELAY) == pdFALSE) continue;
 
-    if(xSemaphoreTake(rasp_recv_data_semaphore, portMAX_DELAY) == pdFALSE) 
-    {
-      xSemaphoreGive(stm32_recv_data_semaphore);
-      continue;
-    }
+    stm32_status = stm32RecieveData.status;
+
+    xSemaphoreGive(stm32_recv_data_semaphore);
+
+    if(xSemaphoreTake(rasp_recv_data_semaphore, portMAX_DELAY) == pdFALSE) continue;
+
+    rasp_cmd = raspCmd;
+    dest_point = destPoint;
+    tag_angle = tagAngle;
+
+    xSemaphoreGive(rasp_recv_data_semaphore);
+
+    tag_position = tagPosition;
 
     //if(xSemaphoreTake(stm32_send_flag_semaphore, 0) == pdFALSE) continue;
 
     Serial.println("stm32SendTask");
 
-    if (tag_position == target_loc || rasp_cmd == CMD_STOP || stm32_recieve_data.status == 0x01) 
+    if (tag_position == dest_point || rasp_cmd == CMD_STOP || stm32_status == 0x00) 
     {
-      stm32_send_data.cmd = CMD_STOP;
+      stm32SendData.cmd = CMD_STOP;
     }
-    else if (std::abs(tag_angle - getAngle(target_loc, tag_position)) < ANGLE_EPSILON) 
+    else if (std::abs(tag_angle - getAngle(dest_point, tag_position)) < ANGLE_EPSILON) 
     {
-      stm32_send_data.cmd = CMD_FORWARD;
+      stm32SendData.cmd = CMD_FORWARD;
+      Serial.println("FORWARD");
     }
-    else if (tag_angle < getAngle(target_loc, tag_position)) 
+    else if (tag_angle < getAngle(dest_point, tag_position)) 
     {
-      stm32_send_data.cmd = CMD_CLOCKWISE_ROTATE;
+      stm32SendData.cmd = CMD_CLOCKWISE_ROTATE;
+      Serial.println("CLOCK");
     }
     else 
     {
-      stm32_send_data.cmd = CMD_COUNTERCLOCKWISE_ROTATE;
+      stm32SendData.cmd = CMD_COUNTERCLOCKWISE_ROTATE;
+      Serial.println("ANTT-CLOCK");
     }
 
     // stm32_send_data.cmd = CMD_COUNTERCLOCKWISE_ROTATE;
-    if(tag_position.z < 1) stm32_send_data.cmd = CMD_FORWARD;
-    else stm32_send_data.cmd = CMD_STOP;
+    // if(tag_position.z < 1) stm32_send_data.cmd = CMD_FORWARD;
+    // else stm32_send_data.cmd = CMD_STOP;
 
     /* Send CMD to STM32 */
-    Stm32HwSerial.write((const uint8_t*)&stm32_send_data, sizeof(STM32SendData));
-
-    xSemaphoreGive(stm32_recv_data_semaphore);
-
-    xSemaphoreGive(rasp_recv_data_semaphore);
+    Stm32HwSerial.write((const uint8_t*)&stm32SendData, sizeof(STM32SendData));
 
     /* Posting to RTLS Task */
     xTaskNotifyGive(rasp_send_task_handle);
@@ -111,5 +121,5 @@ float getAngle(Point2D target, Point2D cur)
   float dx = target.x - cur.x;
   float dz = target.z - cur.z;
 
-  return std::atan2(dz, dx) * 180 / PI;
+  return ((int)(std::atan2(dz, dx) * 180 / PI) + 360 ) % 360;
 }
