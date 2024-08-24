86a27e7c803004252c1fe8b6533e96b752bc35b7
DevSWYoon <62388909+DevSWYoon@users.noreply.github.com>
2024-08-24 17:49:35 +0900

    Update rasp.h
    add CRC for UART

--- esp32-tag-DW3000/include/rasp.h
+++ esp32-tag-DW3000/include/rasp.h
@@ -7,22 +7,25 @@
 #define RASP_RX_PIN 39
 #define RASP_TX_PIN 26
 
+#pragma pack(push, 1)
+
 typedef struct {
-    uint8_t start_byte;
     uint8_t cmd;
     float dest_x;
     float dest_z;
     float angle;
-    uint8_t end_byte;
+    uint8_t crc; 
 } RaspRecvData;
 
 typedef struct {
-    uint8_t start_byte;
     uint8_t stat;
     float loc_x;
     float loc_z;
-    uint8_t end_byte;
+    uint8_t crc; 
 } RaspSendData;
+
+#pragma pack(pop)
+
 
 void raspRecvTask(void *parameter);
 
