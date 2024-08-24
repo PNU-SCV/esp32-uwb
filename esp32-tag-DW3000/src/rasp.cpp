3d8b2977788e79907081907bfa9d0b6c7ab52d6a
DevSWYoon <62388909+DevSWYoon@users.noreply.github.com>
2024-08-24 17:49:12 +0900

    Update rasp.cpp
    add CRC for UART

--- esp32-tag-DW3000/src/rasp.cpp
+++ esp32-tag-DW3000/src/rasp.cpp
@@ -2,6 +2,7 @@
 #include <HardwareSerial.h>
 #include "freertos/FreeRTOS.h"
 #include "rasp.h"
+#include "utils.h"
 
 const uint8_t raspStartByte = 0x02;
 const uint8_t raspEndByte = 0x07;
@@ -28,91 +29,85 @@ float tagAngle = 0.0;
 uint8_t raspCmd = 0x00;
 Point2D destPoint = {3.0, 2.0};
 
-// // 수신 태스크
+// 수신 태스크
 void raspRecvTask(void *parameter) 
 {
-  while (true) 
-  {
-    // Pending for RTLS Task's notification
-    ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
-
-    Serial.println("raspRecvTask");
-
-    uint8_t start_byte, end_byte;
-
-    if (RaspHwSerial.available() >= sizeof(RaspRecvData)) 
+    while (true) 
     {
-      RaspHwSerial.readBytes((char*)&raspRecieveData, sizeof(RaspRecvData));
+        ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
 
-      Serial.println("Data received from Raspberry Pi");
+        Serial.println("raspRecvTask");
 
-      if(xSemaphoreTake(rasp_recv_data_semaphore, portMAX_DELAY) == pdFALSE) continue;
+        if (RaspHwSerial.available() >= sizeof(RaspRecvData)) 
+        {
+            RaspHwSerial.readBytes((char*)&raspRecieveData, sizeof(RaspRecvData));
+            uint8_t receivedCRC = raspRecieveData.crc;
+            uint8_t calculatedCRC = calculateCRC((uint8_t*)&raspRecieveData, sizeof(RaspRecvData) - 1);
 
-      /* Setting target_angle, target_loc */
-      start_byte = raspRecieveData.start_byte;
+            if (receivedCRC == calculatedCRC) 
+            {
+                Serial.println("Data received from Raspberry Pi");
 
-      destPoint = {raspRecieveData.dest_x, raspRecieveData.dest_z};
-      tagAngle = raspRecieveData.angle;
-      raspCmd = raspRecieveData.cmd;
+                if(xSemaphoreTake(rasp_recv_data_semaphore, portMAX_DELAY) == pdFALSE) continue;
 
-      end_byte = raspRecieveData.end_byte;
+                destPoint = {raspRecieveData.dest_x, raspRecieveData.dest_z};
+                tagAngle = raspRecieveData.angle;
+                raspCmd = raspRecieveData.cmd;
 
-      xSemaphoreGive(rasp_recv_data_semaphore);
+                xSemaphoreGive(rasp_recv_data_semaphore);
 
-      if(start_byte != raspStartByte || end_byte != raspEndByte) continue;
-
-      Serial.print("RASP : ");
-      Serial.print(raspCmd);
-      Serial.print(", ");
-      Serial.print(destPoint.x);
-      Serial.print(", ");
-      Serial.print(destPoint.z);
-      Serial.print(", ");
-      Serial.println(tagAngle);
+                Serial.print("RASP : ");
+                Serial.print(raspCmd);
+                Serial.print(", ");
+                Serial.print(destPoint.x);
+                Serial.print(", ");
+                Serial.print(destPoint.z);
+                Serial.print(", ");
+                Serial.println(tagAngle);
+            } 
+            else 
+            {
+                Serial.println("CRC mismatch, data ignored");
+            }
+        }
     }
-  }
 }
+
 
 // 송신 태스크
 void raspSendTask(void *parameter) 
 {
-  uint8_t stm32_status;
+    uint8_t stm32_status;
 
-  while (true) 
-  {
-    // Task pending (RTLS_Task에서 이 태스크를 트리거할 때까지 대기)
-    ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+    while (true) 
+    {
+        ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
 
-    if(xSemaphoreTake(stm32_recv_data_semaphore, portMAX_DELAY) == pdFALSE) continue;
+        if(xSemaphoreTake(stm32_recv_data_semaphore, portMAX_DELAY) == pdFALSE) continue;
 
-    stm32_status = stm32Status;
+        stm32_status = stm32Status;
 
-    xSemaphoreGive(stm32_recv_data_semaphore);
+        xSemaphoreGive(stm32_recv_data_semaphore);
 
-    Serial.println("raspSendTask");
+        Serial.println("raspSendTask");
 
-    raspSendData.start_byte = raspStartByte;
+        raspSendData.stat = stm32_status;
+        raspSendData.loc_x = tagPosition.x;
+        raspSendData.loc_z = tagPosition.z;
 
-    raspSendData.stat = stm32_status;
+        raspSendData.crc = calculateCRC((uint8_t*)&raspSendData, sizeof(RaspSendData) - 1);
 
-    raspSendData.loc_x = tagPosition.x;
-    raspSendData.loc_z = tagPosition.z;
-    
-    raspSendData.end_byte = raspEndByte;
+        Serial.print("Sending data: stat=");
+        Serial.print(raspSendData.stat);
+        Serial.print(", loc_x=");
+        Serial.print(raspSendData.loc_x);
+        Serial.print(", loc_z=");
+        Serial.println(raspSendData.loc_z);
 
-    Serial.print("Sending data: stat=");
-    Serial.print(raspSendData.stat);
-    Serial.print(", loc_x=");
-    Serial.print(raspSendData.loc_x);
-    Serial.print(", loc_z=");
-    Serial.println(raspSendData.loc_z);
+        RaspHwSerial.write((const uint8_t*)&raspSendData, sizeof(RaspSendData));
 
-    // 데이터 송신
-    RaspHwSerial.write((const uint8_t*)&raspSendData, sizeof(RaspSendData));
+        Serial.println("Data sent to Raspberry Pi");
 
-    Serial.println("Data sent to Raspberry Pi");
-
-    // Posting to stm32 Recv Task
-    xTaskNotifyGive(RTLS_task_handle);
-  }
+        xTaskNotifyGive(RTLS_task_handle);
+    }
 }
