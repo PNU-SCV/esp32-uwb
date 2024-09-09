fabb6a5ee1070015bc84075a69c962750a067c8a
DevSWYoon <62388909+DevSWYoon@users.noreply.github.com>
2024-09-09 17:26:38 +0900

    Update stm32.cpp
    Fixed: angle difference algorithm's error

--- esp32-tag-DW3000/src/stm32.cpp
+++ esp32-tag-DW3000/src/stm32.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <HardwareSerial.h>
 #include "freertos/FreeRTOS.h"
+#include <cmath>
 #include "rtls.h"
 #include "rasp.h"
 #include "stm32.h"
@@ -60,7 +61,7 @@ void stm32RecvTask(void *parameter)
       xSemaphoreGive(stm32_recv_data_semaphore);
 
       Serial.print("STM32 Recv: ");
-      Serial.println(stm32Status);
+      Serial.println(stm32RecieveData.status);
     }
   }
 }
@@ -72,6 +73,8 @@ void stm32SendTask(void *parameter)
   Point2D tag_position, dest_point;
   float tag_angle;
 
+  float angle_diff;
+
   while (true) 
   {
     /* Task pending (raspRecvTask에서 이 태스크를 트리거할 때까지 대기) */
@@ -80,6 +83,7 @@ void stm32SendTask(void *parameter)
     if(xSemaphoreTake(stm32_recv_data_semaphore, portMAX_DELAY) == pdFALSE) continue;
 
     stm32_status = stm32RecieveData.status;
+    stm32_status = 0;
 
     xSemaphoreGive(stm32_recv_data_semaphore);
 
@@ -93,21 +97,19 @@ void stm32SendTask(void *parameter)
 
     tag_position = tagPosition;
 
-    //if(xSemaphoreTake(stm32_send_flag_semaphore, 0) == pdFALSE) continue;
+    angle_diff = getAngleDiff(getAngle(dest_point, tag_position), tag_angle);
 
-    Serial.println("stm32SendTask");
-
-    if (tag_position == dest_point || rasp_cmd == CMD_STOP || stm32_status != 0x00) 
-    {
+    if(std::isnan(tag_position.x) || std::isnan(tag_position.z) || tag_position == dest_point || rasp_cmd == CMD_STOP || stm32_status != 0x00) 
+    {    
       stm32SendData.cmd = CMD_STOP;
       Serial.println("STOP");
     }
-    else if (std::abs(tag_angle - getAngle(dest_point, tag_position)) < ANGLE_EPSILON) 
+    else if (angle_diff < ANGLE_EPSILON || (360 - angle_diff) < ANGLE_EPSILON) 
     {
       stm32SendData.cmd = CMD_FORWARD;
       Serial.println("FORWARD");
     }
-    else if (tag_angle < getAngle(dest_point, tag_position)) 
+    else if (angle_diff < 180) 
     {
       stm32SendData.cmd = CMD_CLOCKWISE_ROTATE;
       Serial.println("CLOCK");
@@ -117,6 +119,10 @@ void stm32SendTask(void *parameter)
       stm32SendData.cmd = CMD_COUNTERCLOCKWISE_ROTATE;
       Serial.println("ANTT-CLOCK");
     }
+
+    //if(xSemaphoreTake(stm32_send_flag_semaphore, 0) == pdFALSE) continue;
+
+    Serial.println("stm32SendTask");
 
     // stm32_send_data.cmd = CMD_COUNTERCLOCKWISE_ROTATE;
     // if(tag_position.z < 1) stm32_send_data.cmd = CMD_FORWARD;
