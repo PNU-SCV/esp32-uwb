02849cf319bcf79b310f759c8179c8f172a2f38c
DevSWYoon <62388909+DevSWYoon@users.noreply.github.com>
2024-09-23 14:31:03 +0900

    Update utils.cpp
    Snapshot of Successful Tested Code (09.22 ver)

--- esp32-tag-DW3000/src/utils.cpp
+++ esp32-tag-DW3000/src/utils.cpp
@@ -4,26 +4,30 @@
 
 float getAngle(Point2D target, Point2D cur) 
 {
-  float dx = target.x - cur.x;
-  float dz = target.z - cur.z;
+    float dx = target.x - cur.x;
+    float dz = target.z - cur.z;
 
-  return ((int)(std::atan2(dz, dx) * 180.0f / PI) + 360 ) % 360;
+    float angle = atan2f(dz, dx) * 180.0f / PI;
+    angle = fmodf(angle + 360.0f, 360.0f);
+    return angle;
 }
 
 float getAngleDiff(float dest_angle, float tag_angle)
 {
-    return (int)(dest_angle - tag_angle + 360) % 360;
+    float diff = dest_angle - tag_angle;
+    diff = fmodf(diff + 360.0f, 360.0f);
+    return diff;
 }
 
 uint8_t calculateCRC(const uint8_t* data, size_t length) {
-    uint8_t crc = 0x00; // 초기 CRC 값
+    uint8_t crc = 0x00; 
 
     for (size_t i = 0; i < length; i++) {
-        crc ^= data[i]; // 데이터와 CRC를 XOR 연산
+        crc ^= data[i]; 
 
         for (uint8_t j = 0; j < 8; j++) {
             if (crc & 0x80) {
-                crc = (crc << 1) ^ 0x07; // 다항식 x^8 + x^2 + x + 1 사용
+                crc = (crc << 1) ^ 0x07; 
             } else {
                 crc <<= 1;
             }
