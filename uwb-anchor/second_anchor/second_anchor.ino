00d6dd670bbb28466e40210470a982058534ab3b
DevSWYoon <62388909+DevSWYoon@users.noreply.github.com>
2024-10-22 10:24:53 +0900

    Update second_anchor.ino

--- uwb-anchor/second_anchor/second_anchor.ino
+++ uwb-anchor/second_anchor/second_anchor.ino
@@ -8,43 +8,42 @@ extern SPISettings _fastSPI;
 #define PIN_IRQ 34
 #define PIN_SS 4
 
-#define TX_ANT_DLY 16372// 16385
-#define RX_ANT_DLY 16372 // 16385
+#define TX_ANT_DLY 16372
+#define RX_ANT_DLY 16372
 #define ALL_MSG_COMMON_LEN 10
 #define ALL_MSG_SN_IDX 2
 #define RESP_MSG_POLL_RX_TS_IDX 10
 #define RESP_MSG_RESP_TX_TS_IDX 14
 #define RESP_MSG_TS_LEN 4
-#define POLL_RX_TO_RESP_TX_DLY_UUS 640 // Tag SIde Tx_Rx_DLY + Resp_Rx_TIMEOUT
+#define POLL_RX_TO_RESP_TX_DLY_UUS 640 
 
 #define FRAME_CYCLE_TIME 500
 #define TIME_SLOT_LENGTH 100
 #define TIME_SLOT_COUNT 1
 
-/* Default communication configuration. We use default non-STS DW mode. */
+#define ANCHOR_CHAR 'B'
+
 static dwt_config_t config = {
-    5,                /* Channel number. */
-    DWT_PLEN_128,     /* Preamble length. Used in TX only. */
-    DWT_PAC8,         /* Preamble acquisition chunk size. Used in RX only. */
-    9,                /* TX preamble code. Used in TX only. */
-    9,                /* RX preamble code. Used in RX only. */
-    1,                /* 0 to use standard 8 symbol SFD, 1 to use non-standard 8 symbol, 2 for non-standard 16 symbol SFD and 3 for 4z 8 symbol SDF type */
-    DWT_BR_6M8,       /* Data rate. */
-    DWT_PHRMODE_STD,  /* PHY header mode. */
-    DWT_PHRRATE_STD,  /* PHY header rate. */
-    (129 + 8 - 8),    /* SFD timeout (preamble length + 1 + SFD length - PAC size). Used in RX only. */
-    DWT_STS_MODE_OFF, /* STS disabled */
-    DWT_STS_LEN_64,   /* STS length see allowed values in Enum dwt_sts_lengths_e */
-    DWT_PDOA_M0       /* PDOA mode off */
+    5,                
+    DWT_PLEN_128,     
+    DWT_PAC8,         
+    9,                
+    9,                
+    1,                
+    DWT_BR_6M8,       
+    DWT_PHRMODE_STD,  
+    DWT_PHRRATE_STD,  
+    (129 + 8 - 8),    
+    DWT_STS_MODE_OFF, 
+    DWT_STS_LEN_64,   
+    DWT_PDOA_M0       
 };
 
 static uint8_t rx_time_sync_msg[] = {0x41, 0x88, 0, 0xCA, 0xDE, 'T', 'S', 'Y', 'N', 0xE0, 0, 0};
 
-                              /*   0,    1, 2,    3,    4,   5,   6,   7,   8,  9,    10 */
-static uint8_t rx_poll_msg[] = {0x41, 0x88, 0, 0xCA, 0xDE, 'R', 'B', '*', '*', 0xE0, 0, 0};
-static uint8_t tx_resp_msg[] = {0x41, 0x88, 0, 0xCA, 0xDE, '*', '*', 'R', 'B', 0xE1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-// static uint8_t rx_poll_msg[] = {0x41, 0x88, 0, 0xCA, 0xDE, 'R', 'B', 'T', 'A', 0xE0, 0, 0};
-// static uint8_t tx_resp_msg[] = {0x41, 0x88, 0, 0xCA, 0xDE, 'T', 'A', 'R', 'B', 0xE1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+                  
+static uint8_t rx_poll_msg[] = {0x41, 0x88, 0, 0xCA, 0xDE, 'R', ANCHOR_CHAR, '*', '*', 0xE0, 0, 0};
+static uint8_t tx_resp_msg[] = {0x41, 0x88, 0, 0xCA, 0xDE, '*', '*', 'R', ANCHOR_CHAR, 0xE1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 static uint8_t frame_seq_nb = 0;
 static uint8_t rx_buffer[20];
 static uint32_t status_reg = 0;
@@ -62,9 +61,9 @@ void setup()
   spiBegin(PIN_IRQ, PIN_RST);
   spiSelect(PIN_SS);
 
-  delay(2); // Time needed for DW3000 to start up (transition from INIT_RC to IDLE_RC, or could wait for SPIRDY event)
+  delay(2); 
 
-  while (!dwt_checkidlerc()) // Need to make sure DW IC is in IDLE_RC before proceeding
+  while (!dwt_checkidlerc()) 
   {
     UART_puts("IDLE FAILED\r\n");
     while (1)
@@ -78,26 +77,20 @@ void setup()
       ;
   }
 
-  // Enabling LEDs here for debug so that for each TX the D1 LED will flash on DW3000 red eval-shield boards.
   dwt_setleds(DWT_LEDS_ENABLE | DWT_LEDS_INIT_BLINK);
 
-  /* Configure DW IC. See NOTE 6 below. */
-  if (dwt_configure(&config)) // if the dwt_configure returns DWT_ERROR either the PLL or RX calibration has failed the host should reset the device
+  if (dwt_configure(&config)) 
   {
     UART_puts("CONFIG FAILED\r\n");
     while (1)
       ;
   }
 
-  /* Configure the TX spectrum parameters (power, PG delay and PG count) */
   dwt_configuretxrf(&txconfig_options);
 
-  /* Apply default antenna delay value. See NOTE 2 below. */
   dwt_setrxantennadelay(RX_ANT_DLY);
   dwt_settxantennadelay(TX_ANT_DLY);
 
-  /* Next can enable TX/RX states output on GPIOs 5 and 6 to help debug, and also TX/RX LEDs
-   * Note, in real low power applications the LEDs should not be used. */
   dwt_setlnapamode(DWT_LNA_ENABLE | DWT_PA_ENABLE);
 
   Serial.println("Range TX");
@@ -106,7 +99,6 @@ void setup()
 
 void loop()
 {
-  /* Activate reception immediately. */
   dwt_rxenable(DWT_START_RX_IMMEDIATE);
 
   while (!((status_reg = dwt_read32bitreg(SYS_STATUS_ID)) & (SYS_STATUS_RXFCG_BIT_MASK | SYS_STATUS_ALL_RX_ERR)));
@@ -123,45 +115,37 @@ void loop()
       dwt_readrxdata(rx_buffer, frame_len, 0);
 
       rx_buffer[ALL_MSG_SN_IDX] = 0;
-      if (memcmp(rx_buffer, rx_poll_msg, 7) == 0 && rx_buffer[9] == 0xE0) /* (memcmp(rx_buffer, rx_poll_msg, ALL_MSG_COMMON_LEN) == 0) */
+      if (memcmp(rx_buffer, rx_poll_msg, 7) == 0 && rx_buffer[9] == 0xE0) 
       {
         uint32_t resp_tx_time;
         int ret;
 
         tx_resp_msg[5] = rx_buffer[7];
         tx_resp_msg[6] = rx_buffer[8];
-
-        /* Retrieve poll reception timestamp. */
+        
         poll_rx_ts = get_rx_timestamp_u64();
 
-        /* Compute response message transmission time. See NOTE 7 below. */
         resp_tx_time = (poll_rx_ts + (POLL_RX_TO_RESP_TX_DLY_UUS * UUS_TO_DWT_TIME)) >> 8;
         dwt_setdelayedtrxtime(resp_tx_time);
 
-        /* Response TX timestamp is the transmission time we programmed plus the antenna delay. */
         resp_tx_ts = (((uint64_t)(resp_tx_time & 0xFFFFFFFEUL)) << 8) + TX_ANT_DLY;
 
-        /* Write all timestamps in the final message. See NOTE 8 below. */
         resp_msg_set_ts(&tx_resp_msg[RESP_MSG_POLL_RX_TS_IDX], poll_rx_ts);
         resp_msg_set_ts(&tx_resp_msg[RESP_MSG_RESP_TX_TS_IDX], resp_tx_ts);
 
-        /* Write and send the response message. See NOTE 9 below. */
         tx_resp_msg[ALL_MSG_SN_IDX] = frame_seq_nb;
-        dwt_writetxdata(sizeof(tx_resp_msg), tx_resp_msg, 0); /* Zero offset in TX buffer. */
-        dwt_writetxfctrl(sizeof(tx_resp_msg), 0, 1);          /* Zero offset in TX buffer, ranging. */
+        dwt_writetxdata(sizeof(tx_resp_msg), tx_resp_msg, 0); 
+        dwt_writetxfctrl(sizeof(tx_resp_msg), 0, 1);          
         ret = dwt_starttx(DWT_START_TX_DELAYED);
 
         if (ret == DWT_SUCCESS)
         {
-          /* Poll DW IC until TX frame sent event set. See NOTE 6 below. */
           while (!(dwt_read32bitreg(SYS_STATUS_ID) & SYS_STATUS_TXFRS_BIT_MASK))
           {
           };
 
-          /* Clear TXFRS event. */
           dwt_write32bitreg(SYS_STATUS_ID, SYS_STATUS_TXFRS_BIT_MASK);
 
-          /* Increment frame sequence number after transmission of the poll message (modulo 256). */
           frame_seq_nb = (frame_seq_nb + 1) % 256;
         }
       }
@@ -169,7 +153,6 @@ void loop()
   }
   else
   {
-    /* Clear RX error events in the DW IC status register. */
     dwt_write32bitreg(SYS_STATUS_ID, SYS_STATUS_ALL_RX_ERR);
   }
 }
