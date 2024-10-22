9f63d460951046f9b79410fed0c759a6f6406941
DevSWYoon <62388909+DevSWYoon@users.noreply.github.com>
2024-10-22 10:17:51 +0900

    Update main.cpp

--- esp32-tag-DW3000/src/main.cpp
+++ esp32-tag-DW3000/src/main.cpp
@@ -1,3 +1,25 @@
+/*
+ * Copyright 2024 seokwon Yoon
+ *
+ * This work includes code from the DW3000 library developed by Yannick SILVA.
+ * 
+ * Author: Yannick SILVA <yannick.silva@gmail.com>
+ * Maintainer: Yannick SILVA <yannick.silva@gmail.com>
+ * Original source: https://nconcepts.fr
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include <HardwareSerial.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
